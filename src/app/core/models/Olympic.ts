204415035a7e6bb486446c83f2e8c220c936611a
Nicolas OULIKHANOW <nicolasoulikhanow@OuliMacM3.local>
2024-07-05 19:40:22 +0200

    Create the data models Olympic and Participation

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,9 +1,10 @@
-// TODO: create here a typescript interface for an olympic country
-/*
-example of an olympic country:
-{
-    id: 1,
-    country: "Italy",
-    participations: []
+import { Participation } from "./Participation";
+
+export class OlympicCountry {
+
+    constructor(
+        public id: number,
+        public country: string,
+        public participations: Participation[]
+    ) {}
 }
-*/
